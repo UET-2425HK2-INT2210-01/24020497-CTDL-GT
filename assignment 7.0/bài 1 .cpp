1e5749e6b5a8a489b5cb717b76b07d1c0961358e
24020497-CTDL-GT <24020497@vnu.edu.vn>
2025-04-18 17:42:55 +0700

    Update bài 1 .cpp

--- assignment 7.0/bài 1 .cpp
+++ assignment 7.0/bài 1 .cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int sum = 0;
+int sum = 0;//khai báo tổng ở cục bộ(không bị ảnh hưởng bởi đệ quy)
 int recursion(int n) {
-    if (n == 0) return sum;
-    sum += n;
-    n--;
-    recursion(n);
+    if (n == 0) return sum;//Điều kiện dừng đệ quy
+    sum += n;//Tổng cộng với n
+    n--;//n trừ dần
+    recursion(n);//Đệ quy
 }
 
 int main() {
     int n;
-    cin >> n;
-    cout << recursion(n);
+    cin >> n;//Input
+    cout << recursion(n);//Output
     return 0;
 }
